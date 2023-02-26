f40130f4aceab4d013562798f8e646e6f8d7a954
reza2aulia <36034153+reza2aulia@users.noreply.github.com>
2023-02-26 21:33:25 +0700

    First Commit

--- src/main/java/com/juaracoding/selenium/TwoSelenium.java
+++ src/main/java/com/juaracoding/selenium/TwoSelenium.java
@@ -2,6 +2,7 @@ package com.juaracoding.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
